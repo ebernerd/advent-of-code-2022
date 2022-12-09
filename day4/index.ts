b186bf79473b43f7e5298acb15cf308ad38d9ecd
ebernerd <eric99b@gmail.com>
2022-12-08 21:20:36 -0500

    Day 4!

---
+++ day4/index.ts
@@ -0,0 +1,33 @@
+import { readFileLinesToArray } from "../utils"
+const lines = readFileLinesToArray(`${__dirname}/input.txt`)
+
+type Range = [min: number, max: number]
+
+//	Checks if the entirety of the `needle` range exists within the `haystack` range
+const isRangeInsideRange = (needle: Range, haystack: Range) =>
+	needle[0] >= haystack[0] && needle[1] <= haystack[1]
+
+//	Checks if either of the given ranges contains the other in its entirety
+const checkForWholeOverlap = (range1: Range, range2: Range) =>
+	isRangeInsideRange(range1, range2) || isRangeInsideRange(range2, range1)
+
+//	Checks if any part of one range is inside of the other
+const checkForPartialOverlap = (range1: Range, range2: Range) =>
+	range1[1] >= range2[0] && range2[1] >= range1[0]
+
+let p1sum: number = 0
+let p2sum: number = 0
+for (const line of lines) {
+	const [range1, range2] = line
+		//	Split each range of sections into separate itmes
+		.split(",")
+		//	Then convert that range into a Range object by separating the numbers (by splitting on "-") then converting
+		//	the strings numbers.
+		.map((elf) => elf.split("-").map((v) => Number(v)) as Range)
+
+	p1sum += checkForWholeOverlap(range1, range2) ? 1 : 0
+	p2sum += checkForPartialOverlap(range1, range2) ? 1 : 0
+}
+
+console.log(`Part 1: ${p1sum}`)
+console.log(`Part 2: ${p2sum}`)
